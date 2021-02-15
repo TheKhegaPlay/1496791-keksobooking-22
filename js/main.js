015b18ffae8df8c7023e8ea808d1a72daa2a4bc1
Владислав <vladik.2015.31@gmail.com>
2021-02-15 21:55:58 +0500

    Исправил ошибки №4

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 function getRandomIntInclusive (min, max) {
 
-  if (min < 0 || max < 0 || min < 0, max < 0) {
-    min = Math.abs(min);
-    max = Math.abs(max);
-  }
+  min = Math.abs(min);
+  max = Math.abs(max);
   if (min > max) {
     [min, max] = [max, min];
   }
@@ -13,14 +11,13 @@ getRandomIntInclusive(1, 8);
 
 let calculatingNumberDecimalPlaces = function (min, max, signs) {
 
-  if (min < 0 || max < 0 || min < 0 && max < 0) {
-    min = Math.abs(min);
-    max = Math.abs(max);
-  }
+  min = Math.abs(min);
+  max = Math.abs(max);
+
   if (min > max) {
     [min, max] = [max, min];
   }
-  return Math.random() * (max - min + 1) + min.toFixed(signs);
+  return (Math.random() * (max - min + 1) + min).toFixed(signs);
 };
 
 calculatingNumberDecimalPlaces(2, 8, 5);
