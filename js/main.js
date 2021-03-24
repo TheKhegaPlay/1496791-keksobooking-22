858c2c00b50be7a53e3e87bfdabfd3cc98f19d8b
Владислав <vladik.2015.31@gmail.com>
2021-03-25 01:18:01 +0500

    ДЗ №3

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getRandomIntInclusive (min, max) {
 }
 getRandomIntInclusive(1, 8);
 
-let calculatingNumberDecimalPlaces = function (min, max, signs) {
+const calculatingNumberDecimalPlaces = function (min, max, signs) {
 
   min = Math.abs(min);
   max = Math.abs(max);
@@ -18,6 +18,41 @@ let calculatingNumberDecimalPlaces = function (min, max, signs) {
     [min, max] = [max, min];
   }
   return (Math.random() * (max - min + 1) + min).toFixed(signs);
-};
+}
 
 calculatingNumberDecimalPlaces(2, 8, 5);
+
+const getRandom = function () {
+  return Math.floor(Math.abs(Math.random));
+}
+
+const type = [palace, flat, house, bungalow];
+
+const time = ['12:00', '13:00', '14:00'];
+
+let objFirst = {
+  author:{
+    avatar: 'img/avatars/user0',
+  },
+  offer: {
+    title: 'Первый',
+    price: getRandom,
+    type: type[getRandomIntInclusive(0, 3)],
+    rooms: getRandom,
+    guests: getRandom,
+    checkin: time[getRandomIntInclusive(0, 2)],
+    checkout: time[getRandomIntInclusive(0, 2)],
+
+    description: 'Уютное и теплое жилище',
+
+  },
+  location: {
+    x: calculatingNumberDecimalPlaces(35.65000, 35.70000, 5),
+    y: calculatingNumberDecimalPlaces(139.70000, 139.80000, 5),
+  },
+};
+
+
+
+let dataTest = [objFirst, objSecond, objThird, objFourth, objFifth, objSixth, objSeventh, objEighth, objNinth, objTenth];
+
